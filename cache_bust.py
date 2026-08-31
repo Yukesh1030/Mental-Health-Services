3bc32904a4d62eefabb5b4a3639f0d9ba61c1de7
Yukesh1030 <yukeshyuki18@gmail.com>
2026-08-31 13:15:28 +0530

    Updatde

--- cache_bust.py
+++
@@ -1,9 +0,0 @@
-﻿import re
-
-with open('Login.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = re.sub(r'src="js/forms\.js.*?"', 'src="js/forms.js?v=2"', content)
-
-with open('Login.html', 'w', encoding='utf-8') as f:
-    f.write(content)
